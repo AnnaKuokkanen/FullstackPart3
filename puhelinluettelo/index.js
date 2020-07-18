48e2fc441c6d0b74c54549167fee47152f0e706b
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-19 00:27:29 +0300

    teht 3.1

--- puhelinluettelo/index.js
+++ puhelinluettelo/index.js
@@ -1,26 +1,32 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
-let notes = [
-    {
-        name: "Arto Hellas",
-        number: "0312355390",
-        id: 1
-    },
-    {
-        name: "Ada Lovelace",
-        number: "0234833950",
-        id: 2
-    },
-    {
-        name: "Alan Turing",
-        number: "0403850394",
-        id: 3
-    }
-  ]
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
-    response.end(JSON.stringify(notes))
-  })
+let persons = [
+  {
+      name: "Arto Hellas",
+      number: "0312355390",
+      id: 1
+  },
+  {
+      name: "Ada Lovelace",
+      number: "0234833950",
+      id: 2
+  },
+  {
+      name: "Alan Turing",
+      number: "0403850394",
+      id: 3
+  },
+  {
+      name: "Charles Babbage",
+      number: "0394023439",
+      id: 4
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
 
 const port = 3001
 app.listen(port)
