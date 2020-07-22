b7af1890716e8695819edc4904fd5513ade74939
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-23 00:44:50 +0300

    teht 3.13

--- index.js
+++ index.js
@@ -11,63 +11,74 @@ app.use(cors())
 morgan.token('persondata', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :persondata'))
 
-let persons = [
-  {
-      name: "Arto Hellas",
-      number: "0312355390",
-      id: 1
-  },
-  {
-      name: "Ada Lovelace",
-      number: "0234833950",
-      id: 2
-  },
-  {
-      name: "Alan Turing",
-      number: "0403850394",
-      id: 3
-  },
-  {
-      name: "Charles Babbage",
-      number: "0394023439",
-      id: 4
-  },
-  {
-      name: "John von Neumann", 
-      number: "2438872390",
-      id: 5
-  }
-]
+// let persons = [
+//   {
+//       name: "Arto Hellas",
+//       number: "0312355390",
+//       id: 1
+//   },
+//   {
+//       name: "Ada Lovelace",
+//       number: "0234833950",
+//       id: 2
+//   },
+//   {
+//       name: "Alan Turing",
+//       number: "0403850394",
+//       id: 3
+//   },
+//   {
+//       name: "Charles Babbage",
+//       number: "0394023439",
+//       id: 4
+//   },
+//   {
+//       name: "John von Neumann", 
+//       number: "2438872390",
+//       id: 5
+//   }
+// ]
 
 app.get('/api/persons', (req, res) => {
-  let personsArray = []
+  let persons = []
   Person.find({}).then(result => {
     result.forEach(person => {
         console.log('Henkilö', person)
-        personsArray.push(person)
-        console.log('personsArray', personsArray)
+        persons.push(person)
+        console.log('personsArray', persons)
     })
-    res.json(personsArray)
+    res.json(persons)
   })
-  .catch((error) => {
+  .catch(error => {
     console.log('ERROR', error.message)
   });
 })
 
 app.get('/api/persons/:id', (req, res) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    res.json(person)
-  } else {
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      console.log('Löytyi henkilö', person)
+      res.json(person)
+    } 
+  })
+  .catch(error => {
     res.status(404).end()
-  }
+    console.log('ERROR', error.message)
+  });
 })
 
 app.get('/info', (req, res) => {
   const date = new Date().getTime()
-  res.send(`<p>Phonebook has info for ${persons.length} people</p>
+  let people = 0
+  Person.find({}).then(result => {
+    result.forEach(person => {
+      console.log('lisätään')
+      people = people + 1
+      console.log('arvo:', people)
+    })
+    res.send(`<p>Phonebook has info for ${people} people</p>
             <p>Date: ${new Date()}</p>`)
+  })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
