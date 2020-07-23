186ded7721dea6c161ac9768bc82f26571ec02bc
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-23 22:33:37 +0300

    teht 3.16

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 morgan.token('persondata', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :req[content-length] - :response-time ms :persondata'))
 
-app.get('/api/persons', (req, res) => {
+app.get('/api/persons', (req, res, next) => {
   let persons = []
   Person.find({}).then(result => {
     result.forEach(person => {
@@ -19,12 +19,10 @@ app.get('/api/persons', (req, res) => {
     })
     res.json(persons)
   })
-  .catch(error => {
-    console.log('ERROR', error.message)
-  });
+  .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
   .then(person => {
     if (person) {
@@ -37,21 +35,21 @@ app.get('/api/persons/:id', (req, res) => {
   .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
   const date = new Date().getTime()
   let people = 0
-  Person.find({}).then(result => {
+  Person.find({})
+  .then(result => {
     result.forEach(person => {
-      //console.log('lisätään')
       people = people + 1
-      //console.log('arvo:', people)
     })
     res.send(`<p>Phonebook has info for ${people} people</p>
             <p>Date: ${new Date()}</p>`)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
   .then(result => {
     response.status(204).end()
@@ -59,7 +57,7 @@ app.delete('/api/persons/:id', (req, res) => {
   .catch(error => next(error))
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const person = req.body
   if (!person.name || !person.number) {
     return res.status(400).json({ 
@@ -75,7 +73,26 @@ app.post('/api/persons', (req, res) => {
   p.save().then(savedPerson => {
     res.json(savedPerson)
   })
+  .catch(error => next(error))
 })
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
