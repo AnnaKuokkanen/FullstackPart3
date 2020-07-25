29b7a7f33dd29d87d67ac24a9e8843cd53968225
Anna Kuokkanen <kuokkanenanna9@gmail.com>
2020-07-26 01:37:28 +0300

    teht 3.22

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.get('/info', (req, res, next) => {
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id).then(res => {
+  Person.findByIdAndRemove(req.params.id).then(result => {
     res.status(204).end()
   }).catch(error => next(error))
 })
